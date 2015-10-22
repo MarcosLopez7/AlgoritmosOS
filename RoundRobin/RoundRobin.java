cdddecf7eda2c66833199e82650a26f8704b62c9
Erick Rivas <rivadunga@hotmail.com>
2015-10-22 16:48:52 -0500

    Round Robin documentado

--- RoundRobin/RoundRobin.java
+++ RoundRobin/RoundRobin.java
@@ -6,29 +6,40 @@ import java.util.Vector;
 
 public class RoundRobin
 {
+    //NOTA: Para mayor flexibilidad se manejo el programa
+    // por milisegundos
+    public static int TOTAL_TIME     = 0;
+    public static int INTERVAL_TIME  = 500;
+    private       int quantum        = 0;
+    private       int currentProcess = 0;
 
-    public static int TOTAL_TIME    = 0;
-    public static int INTERVAL_TIME = 500;
-    private int             quantum        = 0;
-    private int             currentProcess = 0;
-    private Vector<Integer> grantt         = new Vector<>();
-    private Vector<String>  names          = new Vector<>();
-    private Vector<Integer> start          = new Vector<>();
-    private Vector<Integer> duration       = new Vector<>();
-    private Vector<Integer> wait           = new Vector<>();
-    private Vector<Integer> queue          = new Vector<>();
+
+    //Atributos de los procesos
+    private Vector<String>  names    = new Vector<>();
+    private Vector<Integer> start    = new Vector<>();
+    private Vector<Integer> duration = new Vector<>();
+    private Vector<Integer> wait     = new Vector<>();
+
+    //Vector referente a la cola del thread
+    private Vector<Integer> queue = new Vector<>();
+
+    //Vector referente al diagrama de grantt 
+    private Vector<Integer> grantt = new Vector<>();
+    private int lastDuration = 0;
 
     public RoundRobin()
     {
         readData();
     }
 
+    //Lee datos del archivo procesos.txt
     public void readData()
     {
         List<String> lines = new Vector<>();
         try {
             lines = Files.readAllLines(Paths.get("procesos.txt"));
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
 
         quantum = Integer.parseInt(lines.get(0)) * 1000;
         names.add("");
@@ -46,20 +57,40 @@ public class RoundRobin
         }
     }
 
-    private int lastDuration = 0;
     public void executeAction(int time)
     {
-        for (int i = 1; i < start.size(); i++) {
-            if (start.get(i) == time) {
-                queue.add(0, i);
-                System.out.printf("%06d", time);    //  -->  "00461012"
-                System.out.println(
-                        " <" + currentProcess + "> = " + "Entra " + i + " " + getStrQueue());
+
+        Vector<Integer> newProcesses = new Vector<Integer>();
+        for (int i = 1; i < start.size(); i++)
+            if (start.get(i) == time) newProcesses.add(i);
+
+
+        //Ordena para desempatar en caso de que haya más de un proceso
+        //al mismo tiempo
+        for (int j = 0; j < newProcesses.size(); j++) {
+            for (int i = 0; i < newProcesses.size(); i++) {
+                if (duration.get(newProcesses.get(i)) > duration.get(newProcesses.get(j))) {
+                    int auxI = newProcesses.get(i);
+                    newProcesses.set(i, newProcesses.get(j));
+                    newProcesses.set(j, auxI);
+                }
             }
         }
 
+        //Los agrega ordenados
+        for (int i = 0; i < newProcesses.size(); i++) {
+            queue.add(0, newProcesses.get(i));
+            System.out.printf("%06d", time);    //  -->  "00461012"
+            System.out.println(
+                    " <" + currentProcess + "> = " + "Entra " + newProcesses.get(i) + " " +
+                    getStrQueue());
+        }
+
+
+        //Ejecuta un movimiento en caso de que se termine el quantum o que se termine el proceso actual
         if (duration.get(currentProcess) <= 0 || lastDuration % quantum == 0) {
 
+            //Fin quantum
             if (lastDuration % quantum == 0) {
                 System.out.printf("%06d", time);
                 System.out.println(" <" + currentProcess + "> = Fin quantum");
@@ -67,10 +98,11 @@ public class RoundRobin
 
             if (duration.get(currentProcess) > 0) {
                 queue.add(0, currentProcess);
-            } else {
+            } else { //Fin proceso
                 System.out.printf("%06d", time);
                 System.out.println(" <" + currentProcess + "> = FIN PROCESO " + currentProcess);
             }
+            //Agrega un nuevo proceso
             if (queue.size() != 0) {
                 currentProcess = queue.lastElement();
                 queue.remove(queue.size() - 1);
@@ -79,13 +111,14 @@ public class RoundRobin
                 System.out.println(
                         " <" + currentProcess + "> = NUEVO PROCESO " + currentProcess + " " +
                         getStrQueue());
-            } else {
+            } else { //En caso de que no haya un proceso disponible
                 currentProcess = 0;
                 System.out.printf("%06d", time);
                 System.out.println(" = Sin proceso");
             }
         }
 
+        //Aumento en un intervalo el tiempo de espera
         for (int i = 0; i < queue.size(); i++) {
             wait.set(queue.get(i), wait.get(queue.get(i)) + INTERVAL_TIME);
         }
@@ -95,6 +128,7 @@ public class RoundRobin
         grantt.add(currentProcess);
     }
 
+    //Obtiene un string de la cola para fines de demostracion
     public String getStrQueue()
     {
         String strQueue = " <";
@@ -107,10 +141,12 @@ public class RoundRobin
     }
 
 
+    //Muestra las estadisticas
     public void showStats()
     {
         int auxProc = 0;
         System.out.println();
+        //Gantt
         System.out.println("--- Gantt ---");
         for (int i = 0; i < TOTAL_TIME; i += INTERVAL_TIME) {
             if (auxProc != grantt.get(i / INTERVAL_TIME)) {
@@ -120,6 +156,7 @@ public class RoundRobin
             }
         }
 
+        //Tiempos de espera
         int total = 0;
         System.out.println();
         System.out.println("--- Tiempos de espera ---");
